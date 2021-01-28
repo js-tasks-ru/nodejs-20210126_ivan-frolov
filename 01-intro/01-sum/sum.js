4b5deafd68b9723698cff9ee491e8dc28e294c08
ifrolov <ivanushka.frolov@gmail.com>
2021-01-28 11:43:53 +0300

    add solution for 1-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
-function sum(a, b) {
-  /* ваш код */
+function sum(...arguments) {
+  const TypeErrorText = 'Накосячили с типами';
+
+  return arguments.reduce((acc, num) => {
+    if (typeof num !== 'number') {
+      throw new TypeError(TypeErrorText);
+    }
+    return acc + num;
+  }, 0);
 }
 
 module.exports = sum;
